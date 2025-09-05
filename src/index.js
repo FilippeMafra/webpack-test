9fc2458be63450824e3c4a59c6da1d4d3618f849
Filippe Mafra <c@filippemafra.dev>
2025-09-05 19:45:38 -0300

    feat: :sparkles: add htmlWebpack plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import printMe from './print.js'
+
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
 
   element.innerHTML = 'Hello Webpack'
   element.classList.add('hello')
+
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
 
   return element
 }
