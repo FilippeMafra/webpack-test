4c8201491fd0e3bbbf059d847fe1940ff0efd453
Filippe Mafra <c@filippemafra.dev>
2025-09-06 13:26:02 -0300

    feat: :sparkles: add babel

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
-import printMe from './print.js'
-
 function component() {
   const element = document.createElement('div')
   const btn = document.createElement('button')
 
-  element.innerHTML = 'Hello Webpack'
+  element.innerHTML = 'Hello Webpack + Babel'
   element.classList.add('hello')
-
-  btn.innerHTML = 'Click me and check the console!'
-  btn.onclick = printMe
-  element.appendChild(btn)
 
   return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,16 @@ const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    app: ['./src/app.js'],
   },
-  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : undefined,
   devServer: {
     static: './dist',
   },
@@ -19,13 +23,18 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin(),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
